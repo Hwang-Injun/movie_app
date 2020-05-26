fd2db4c03720c03947c460f5c9240d94c1199d3c
Hwang-Injun <sroo2315@gmail.com>
2020-05-26 20:24:18 +0900

    Updates

---
+++ precache-manifest.da7035f35769dcb6828d7c2a97a11379.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8945c7a0eadab306e7e619488528f156",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "1b67a6b3b532084e9b8c",
+    "url": "/movie_app/static/css/main.6832488b.chunk.css"
+  },
+  {
+    "revision": "78a8c6d4641937053d35",
+    "url": "/movie_app/static/js/2.19735fbb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.19735fbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b67a6b3b532084e9b8c",
+    "url": "/movie_app/static/js/main.1ef08a4b.chunk.js"
+  },
+  {
+    "revision": "793037b19926e1970ea2",
+    "url": "/movie_app/static/js/runtime-main.8469f341.js"
+  }
+]);
