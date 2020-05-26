06b894a8b5239ddec52e6c74c3654af7920420bb
Hwang-Injun <sroo2315@gmail.com>
2020-05-26 22:48:31 +0900

    Movie app finished, link to https://innjuun.github.io/movie_app

---
+++ src/routes/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./About.css";
+function About(props) {
+  return (
+    <div className="about__container">
+      <span>About this page: I build it because I love movies</span>
+      <span>George</span>
+    </div>
+  );
+}
+
+export default About;
